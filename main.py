eb320115f308daf0bb3a1e19f9df0a3ef3d1f2c6
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-06-24 18:17:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -485,7 +485,7 @@ async def account_login(bot: Client, m: Message):
                 prog = await m.reply_text(Show)
                 cc = f'{str(count).zfill(3)}**.** {name1} {res}\n**Batch :-** {raw_text0}'
                 cc1 = f'{str(count).zfill(3)}**.** {name1} {res}.pdf\n**Batch :-** {raw_text0}'
-                                         await prog.delete (True)
+                                         #await prog.delete (True)
                 if cmd == "pdf" or "drive" in url:
                                      try:
                                          ka=await helper.download(url,name)
