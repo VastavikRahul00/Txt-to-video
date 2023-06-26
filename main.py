f656c2af49e0ebf921f3525a259ccac4701e6dad
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-06-26 21:31:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -431,7 +431,7 @@ logger = logging.getLogger()
 #     getstatusoutput(f"wget '{thumb}' -O 'thumb.jpg'")
 #     thumb = "thumb.jpg"
 
-@app.on_message(pyrogram.filters.private & pyrogram.filters.command(["stats","status"]))
+@bot.on_message(pyrogram.filters.private & pyrogram.filters.command(["stats","status"]))
 async def stats(bot, update):
     back = await handle_force_sub(bot, update)
     if back == 400:
