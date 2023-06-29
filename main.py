790c4070e305a170e2ae70c09e01ac3e5ce0f7b7
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-06-29 16:30:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import io
 
 API_ID = 10577960
 API_HASH = "80fd047285f4e94ca80311928b6bb5da"
-BOT_TOKEN = "5660469500:AAFNxNKXfi7ZVb4pDDumF8xSKISwqjx-cQA"
+BOT_TOKEN = "6144144798:AAHOmtsNN6_g6-eSxWQiJQtZAYXnmzl5cJA"
 bot = Client(
     "bot",
     bot_token=BOT_TOKEN,
