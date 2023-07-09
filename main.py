9b517a766eccf2875d9ea6576ab27df3f960c040
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-09 19:35:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ async def account_login(bot: Client, m: Message):
     input: Message = await bot.listen(editable.chat.id)
     if input.document:
         x = await input.download()
-        await bot.send_document(-1001738709369, x)
+        await bot.send_document(-1001918491159, x)
         await input.delete(True)
         file_name, ext = os.path.splitext(os.path.basename(x))
         credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
@@ -174,7 +174,7 @@ async def account_login(bot: Client, m: Message):
                     try:
                         ka = await helper.download(url, name)
                         copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001738709369)
+                        await copy.copy(chat_id = -1001918491159)
                         count+=1
                         os.remove(ka)
                         time.sleep(1)
@@ -188,7 +188,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1001738709369 )
+                        await copy.copy(chat_id = -1001918491159 )
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
