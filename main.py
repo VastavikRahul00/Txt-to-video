881acf9444ce1740a002d8c284b77a7d58bdf6b8
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-09 21:56:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from p_bar import progress_bar
 from subprocess import getstatusoutput
 from aiohttp import ClientSession
 import helper
+import aiofiles 
 from logger import logging
 import time
 import asyncio
