98c5d3d6c3cd1bd2b7f7f838b57b6967c46bf8ac
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-10 16:20:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from pyrogram.errors.exceptions.bad_request_400 import StickerEmojiInvalid
 import requests
 import json
 import subprocess
@@ -8,88 +7,102 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait
 from pyromod import listen
 from pyrogram.types import Message
-from pyrogram import Client, filters
+import pyrogram
+import tgcrypto
 from p_bar import progress_bar
 from subprocess import getstatusoutput
-from aiohttp import ClientSession
 import helper
-from logger import logging
+import logging
 import time
+import glob
+import aiohttp
+from aiohttp import ClientSession
 import asyncio
 from pyrogram.types import User, Message
 import sys
 import re
 import os
+import io
 
-bot = Client("bot",
-             bot_token= "6369053795:AAF9VQBiL85y6XWnxjYa6wpPyM0sV_A6xwc",
-             api_id= 27495136,
-             api_hash= "4ccc4865eec4d8fde7530e71948b3424")
-
+API_ID = 10577960
+API_HASH = "80fd047285f4e94ca80311928b6bb5da"
+BOT_TOKEN = "6133434192:AAHDkrhW9sgqzLU3ay4o7GRy_yPyDuWJdIc"
+bot = Client(
+    "bot",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH
+)
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /covid")
+
+ editable = await m.reply_text("**Hi Press**\n**Text** = /pro_txt\n**Top** = /pro_top\n**Vision** = /pro_vision\n**Jw** = /pro_jw\n**Olive** = /pro_olive\n**Addapdf** = /adda_pdf")
 
 
-@bot.on_message(filters.command("stop"))
+@bot.on_message(filters.command(["cancel"]))
+async def cancel(_, m):
+    editable = await m.reply_text("Canceling All process Plz wait\nðŸš¦ðŸš¦ Last Process Stopped ðŸš¦ðŸš¦")
+    global cancel
+    cancel = True
+    await editable.edit("cancled")
+    return
+
+
+@bot.on_message(filters.command("restart"))
 async def restart_handler(_, m):
-    await m.reply_text("**STOPPED**🛑🛑", True)
+    await m.reply_text("Restarted!", True)
     os.execl(sys.executable, sys.executable, *sys.argv)
 
 
-@bot.on_message(filters.command(["covid"]))
+@bot.on_message(filters.command(["pro_txt"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"**Hey [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nSend txt file**")
+    editable = await m.reply_text('Send TXT in **NAME : LINK** format to download')
     input: Message = await bot.listen(editable.chat.id)
-    if input.document:
-        x = await input.download()
-        await bot.send_document(-1001918491159, x)
-        await input.delete(True)
-        file_name, ext = os.path.splitext(os.path.basename(x))
-        credit = f"[{m.from_user.first_name}](tg://user?id={m.from_user.id})"
+    x = await input.download()
+    await input.delete(True)
 
 
-        path = f"./downloads/{m.chat.id}"
+    path = f"./downloads/{m.chat.id}"
 
-        try:
-            with open(x, "r") as f:
-                content = f.read()
-            content = content.split("\n")
-            links = []
-            for i in content:
-                links.append(i.split("://", 1))
-            os.remove(x)
-            # print(len(links)
-        except:
-            await m.reply_text("Invalid file input.🥲")
-            os.remove(x)
-            return
-    else:
-        content = input.text
+    try:
+        with open(x, "r") as f:
+            content = f.read()
         content = content.split("\n")
         links = []
         for i in content:
             links.append(i.split("://", 1))
-   
-    await editable.edit(f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **1**")
-    input0: Message = await bot.listen(editable.chat.id)
-    raw_text = input0.text
-    await input0.delete(True)
-
-    await editable.edit("**Enter Batch Name**\n**or** \n**send C for default name.**")
+        os.remove(x)
+        # print(len(links))
+    except:
+        await m.reply_text("Invalid file input.")
+        os.remove(x)
+        return
+else: 
+   content = input.text
+        content = content.split("\n")
+        links = []
+        for i in content:
+            links.append(i.split("://", 1))
+            
+    editable = await m.reply_text(
+        f"Total links found are **{len(links)}**\n\nSend From where you want to download initial is **0**"
+    )
     input1: Message = await bot.listen(editable.chat.id)
-    raw_text0 = input1.text
-    await input1.delete(True)
-    if raw_text0 == 'C':
-        b_name = file_name
-    else:
-        b_name = raw_text0
+    raw_text = input1.text
 
-    await editable.edit("**Enter resolution**")
+    try:
+        arg = int(raw_text)
+    except:
+        arg = 0
+
+    editable = await m.reply_text("**Enter Title**")
+    input0: Message = await bot.listen(editable.chat.id)
+    raw_text0 = input0.text
+
+    await m.reply_text("**Enter resolution**")
     input2: Message = await bot.listen(editable.chat.id)
     raw_text2 = input2.text
-    await input2.delete(True)
     try:
         if raw_text2 == "144":
             res = "256x144"
@@ -110,11 +123,12 @@ async def account_login(bot: Client, m: Message):
     
     
 
-    await editable.edit("Now send the **Thumb url**\nEg : ```https://telegra.ph/file/0633f8b6a6f110d34f044.jpg```\n\nor Send `no`")
+
+    editable4 = await m.reply_text(
+        "Now send the **Thumb url**\nEg : ```https://telegra.ph/file/d9e24878bd4aba05049a1.jpg```\n\nor Send **no**"
+    )
     input6 = message = await bot.listen(editable.chat.id)
     raw_text6 = input6.text
-    await input6.delete(True)
-    await editable.delete()
 
     thumb = input6.text
     if thumb.startswith("http://") or thumb.startswith("https://"):
@@ -123,17 +137,17 @@ async def account_login(bot: Client, m: Message):
     else:
         thumb == "no"
 
-    if len(links) == 1:
+    if raw_text == '0':
         count = 1
     else:
         count = int(raw_text)
 
     try:
-        for i in range(count - 1, len(links)):
+        for i in range(arg, len(links)):
 
             V = links[i][1].replace("file/d/","uc?export=download&id=").replace("www.youtube-nocookie.com/embed", "youtu.be").replace("?modestbranding=1", "").replace("/view?usp=sharing","") # .replace("mpd","m3u8")
             url = "https://" + V
-
+             
             if "visionias" in url:
                 async with ClientSession() as session:
                     async with session.get(url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9', 'Accept-Language': 'en-US,en;q=0.9', 'Cache-Control': 'no-cache', 'Connection': 'keep-alive', 'Pragma': 'no-cache', 'Referer': 'http://www.visionias.in/', 'Sec-Fetch-Dest': 'iframe', 'Sec-Fetch-Mode': 'navigate', 'Sec-Fetch-Site': 'cross-site', 'Upgrade-Insecure-Requests': '1', 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX2121) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'sec-ch-ua-mobile': '?1', 'sec-ch-ua-platform': '"Android"',}) as resp:
@@ -159,53 +173,75 @@ async def account_login(bot: Client, m: Message):
                 cmd = f'yt-dlp -o "{name}.mp4" "{url}"'
             else:
                 cmd = f'yt-dlp -f "{ytf}" "{url}" -o "{name}.mp4"'
-
-            try:                               
-                cc = f'**Vid_id  »** {str(count).zfill(3)}\n**Title  »** {name1} {res}🇨‌ 🇴‌ 🇻 🇮 🇩.mkv\n**Batch »** {b_name}\n\n'
-                cc1 = f'**Vid_id  »** {str(count).zfill(3)}\n**Title »** {name1} 🇨‌ 🇴‌ 🇻 🇮 🇩.pdf \n**Batch »** {b_name}\n\n'
-                if "drive" in url:
+     
+                 
+            
+            try:
+                Show = f"**Downloading:-**\n\n**Name :-** `{name}\nQuality - {raw_text2}`\n\n**Url :-** `{url}`"
+                prog = await m.reply_text(Show)
+                cc = f"**Name 📛 »** {name1} {res}💔Marty.mkv\n**Batch 🔖 »** {raw_text0}\n**Index 🗂️ »** {str(count).zfill(3)}"
+                cc1 = f"**Name 📛 Â»** ** {name1} {res}💔Marty.pdf\n**Batch 🔖 »** {raw_text0}\n**Index 🗂️ »** {str(count).zfill(3)}"
+                #                         await prog.delete (True)
+                #                 if cmd == "pdf" or "drive" in url:
+                #                     try:
+                #                         ka=await helper.download(url,name)
+                #                         await prog.delete (True)
+                #                         time.sleep(1)
+                #                         # await helper.send_doc(bot,m,cc,ka,cc1,prog,count,name)
+                #                         reply = await m.reply_text(f"Uploading - `{name}`")
+                #                         time.sleep(1)
+                #                         start_time = time.time()
+                #                         await m.reply_document(ka,caption=cc1)
+                #                         count+=1
+                #                         await reply.delete (True)
+                #                         time.sleep(1)
+                #                         os.remove(ka)
+                #                         time.sleep(3)
+                #                     except FloodWait as e:
+                #                         await m.reply_text(str(e))
+                #                         time.sleep(e.x)
+                #                         continue
+                if cmd == "pdf" or ".pdf" in url or ".pdf" in name:
                     try:
-                        ka = await helper.download(url, name)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
-                        await copy.copy(chat_id = -1001738709369)
-                        count+=1
-                        os.remove(ka)
+                        ka = await helper.aio(url, name)
+                        await prog.delete(True)
                         time.sleep(1)
-                    except FloodWait as e:
-                        await m.reply_text(str(e))
-                        time.sleep(e.x)
-                        continue
-                elif ".pdf" in url:
-                    try:
-                        cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
-                        download_cmd = f"{cmd} -R 25 --fragment-retries 25"
-                        os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1001918491159 )
+                        reply = await m.reply_text(f"Uploading - ```{name}```")
+                        time.sleep(1)
+                        start_time = time.time()
+                        await m.reply_document(
+                            ka,
+                            caption=
+                            f"**Name 📛 »** {name1} {res}💔Marty.pdf\n**Batch 🔖 »** {raw_text0}\n**Index 🗂️ »** {str(count).zfill(3)}"
+                        )
                         count += 1
-                        os.remove(f'{name}.pdf')
+                        # time.sleep(1)
+                        await reply.delete(True)
+                        time.sleep(1)
+                        os.remove(ka)
+                        time.sleep(3)
                     except FloodWait as e:
                         await m.reply_text(str(e))
                         time.sleep(e.x)
                         continue
                 else:
-                    prog = await m.reply_text(f"**Downloading:-**\n\n** Video Name :-** `{name}\nQuality - {raw_text2}`\n**link:**`{url}`")
                     res_file = await helper.download_video(url, cmd, name)
                     filename = res_file
-                    await prog.delete(True)
-                    await helper.send_vid(bot, m, cc, filename, thumb, name, prog)
+                    await helper.send_vid(bot, m, cc, filename, thumb, name,
+                                          prog)
                     count += 1
+                    time.sleep(1)
 
             except Exception as e:
                 await m.reply_text(
-                    f"**downloading failed **\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`"
+                    f"**downloading failed âŒ**\n{str(e)}\n**Name** - {name}\n**Link** - `{url}`"
                 )
-                count += 1
                 continue
 
     except Exception as e:
         await m.reply_text(e)
-    await m.reply_text("🔰Done🔰")
+    await m.reply_text("Done")
+
 
 
 bot.run()
