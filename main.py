9c1c2d17a735aced6d6124982897901d347f6107
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-10 12:29:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -166,7 +166,7 @@ async def account_login(bot: Client, m: Message):
                 if "drive" in url:
                     try:
                         ka = await helper.download(url, name)
-                        copy = await bot.send_document(document=ka, caption=cc1)
+                        copy = await bot.send_document(chat_id=m.chat.id,document=ka, caption=cc1)
                         await copy.copy()
                         count+=1
                         os.remove(ka)
@@ -180,7 +180,7 @@ async def account_login(bot: Client, m: Message):
                         cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(document=f'{name}.pdf', caption=cc1)
+                        copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
                         await copy.copy()
                         count += 1
                         os.remove(f'{name}.pdf')
