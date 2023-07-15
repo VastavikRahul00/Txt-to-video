73b98575e8d2d8db4378349a77923bfc18dc2883
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-15 22:25:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,7 @@ async def account_login(bot: Client, m: Message):
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
 
             elif 'manifest' in url:
-                  cmd = f'yt-dlp -o "{name}.mp4" -f "{ytf}+bestaudio" "{link}"'
+                  cmd = f'yt-dlp -o ".mp4" -f "{ytf}+bestaudio" "{link}"'
                   download_cmd = f"{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args 'aria2c: -x 16 -j 32'"
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
