c90f5cd05e47e20f676077faefa738b45c9f7af1
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-19 23:38:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -134,7 +134,8 @@ async def account_login(bot: Client, m: Message):
 
             elif 'videos.classplusapp' in url:
              url = requests.get(f'https://api.classplusapp.com/cams/uploader/video/jw-signed-url?url={url}', headers={'x-access-token': 'eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgzNjkyMTIsIm9yZ0lkIjoyNjA1LCJ0eXBlIjoxLCJtb2JpbGUiOiI5MTcwODI3NzQyODkiLCJuYW1lIjoiQWNlIiwiZW1haWwiOm51bGwsImlzRmlyc3RMb2dpbiI6dHJ1ZSwiZGVmYXVsdExhbmd1YWdlIjpudWxsLCJjb3VudHJ5Q29kZSI6IklOIiwiaXNJbnRlcm5hdGlvbmFsIjowLCJpYXQiOjE2NDMyODE4NzcsImV4cCI6MTY0Mzg4NjY3N30.hM33P2ai6ivdzxPPfm01LAd4JWv-vnrSxGXqvCirCSpUfhhofpeqyeHPxtstXwe0'}).json()['url']
-
+            elif ".pdf" in url: #and raw_text2 in ["144", "240", "360", "480", "720", "no"]:
+                cmd = f'yt-dlp -o "{name}.pdf" "{url}"'
            
             elif '/master.mpd' in url:
              id =  url.split("/")[-2]
