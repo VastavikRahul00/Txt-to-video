e028ce2b665430b9b5a371ac8f6ebcf4a79b255a
Martyy2003 <131404553+Martyy2003@users.noreply.github.com>
2023-07-20 09:20:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6369053795:AAF9VQBiL85y6XWnxjYa6wpPyM0sV_A6xwc",
+             bot_token= "6318119370:AAHZRO1n-as7qs9CxGztsn78d-pwWpC2r-c",
              api_id= 27495136,
              api_hash= "4ccc4865eec4d8fde7530e71948b3424")
 
