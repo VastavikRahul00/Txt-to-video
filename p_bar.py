e81bb8c3ce648a06b7257b5f5cb70f58153eeb2b
VastavikRahul00 <150103487+VastavikRahul00@users.noreply.github.com>
2023-12-14 20:28:15 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -89,7 +89,7 @@ def hrt(seconds, precision = 0):
 
 timer = Timer()
 
-# designed by 𝙄𝙉𝙑𝙄𝙓
+# designed by Vastavik
 async def progress_bar(current, total, reply, start):
     if timer.can_send():
         now = time.time()
@@ -112,7 +112,7 @@ async def progress_bar(current, total, reply, start):
             
             #don't even change anything till here
             # Calculate progress bar dots
-            #ab mila dil ko sukun #by 𝙄𝙉𝙑𝙄𝙓
+            #ab mila dil ko sukun #by Vastavik
             #change from here if you want 
             bar_length = 11
             completed_length = int(current * bar_length / total)
@@ -120,8 +120,8 @@ async def progress_bar(current, total, reply, start):
             progress_bar = "▰" * completed_length + "▱" * remaining_length
             
             try:
-                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ 🇨‌ 🇴‌ 🇻 🇮 🇩 ⌋────╯`\n') 
-         #       await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 🇨‌ 🇴‌ 🇻 🇮 🇩 ⌋─╯`\n') 
+                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰────⌈ 🇻🇦🇸🇹🇦🇻🇮🇰 ⌋────╯`\n') 
+         #       await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈  𝘽𝙤𝙩 𝙈𝙖𝙙𝙚 𝙗𝙮 🇻🇦🇸🇹🇦🇻🇮🇰 ⌋─╯`\n') 
             except FloodWait as e:
                 time.sleep(e.x)
 
