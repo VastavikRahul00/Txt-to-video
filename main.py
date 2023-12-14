fd73a3aeb1bf6b1733ac710050fb027ff91ee5cb
VastavikRahul00 <150103487+VastavikRahul00@users.noreply.github.com>
2023-12-14 20:48:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ bot = Client("bot",
 
 @bot.on_message(filters.command(["start"]))
 async def account_login(bot: Client, m: Message):
-    editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /covid")
+    editable = await m.reply_text(f"Hello [{m.from_user.first_name}](tg://user?id={m.from_user.id})\nPress /vastavik")
 
 
 @bot.on_message(filters.command("stop"))
